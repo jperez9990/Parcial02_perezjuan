83c134976961aeeda7524ec1a5bd290303fef00c
Juan Francisco <jperez201@cuc.edu.co>
2024-04-30 17:23:00 -0500

    entregable 01

--- src/app/utilidades/rutas/Ruteo.tsx
+++ src/app/utilidades/rutas/Ruteo.tsx
@@ -4,8 +4,7 @@ import { Inicio } from "../../componentes/contenedor/Inicio";
 import { NoEncontrado } from "../../componentes/contenedor/NoEncontrado";
 
 import { ConciertoCrear } from "../../componentes/conisertos/ConciertoCrear";
-import { ConciertoListar } from "../../componentes/conisertos/ConciertoLista";
-import { ConciertoAdmin } from "../../componentes/conisertos/ConciertoAdmin";
+
 
 export const Ruteo = () => {
   return (
@@ -13,8 +12,6 @@ export const Ruteo = () => {
       <Route path="/" element={<Inicio />} />
 
       <Route path="/concre" element={<ConciertoCrear />} />
-      <Route path="/conlis" element={<ConciertoListar />} />
-      <Route path="/conadm" element={<ConciertoAdmin />} />
 
       <Route path="*" element={<NoEncontrado />} />
     </Routes>
